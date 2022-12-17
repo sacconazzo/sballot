67b52fdf88ab269ea5b3c926220003b89ef23d46
Giona Righini <grighini@alteanet.it>
2022-12-17 12:35:46 +0100

    test fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import Winner from './comps/Winner'
 
-test('renders learn react link', async () => {
+test('First test', async () => {
   render(<Winner ballot={{ isOpen: true }} />)
-  const linkElement = screen.getAllByTitle(/Winning/i)
-  expect(linkElement).toBeInTheDocument()
+  const linkElement = screen.getAllByText(/Winning/i)
+  expect(linkElement[0]).toBeVisible()
 })
